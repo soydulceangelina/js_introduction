c0eb5bdf8d533553bb9cbac0ff9fec454cd3e4e5
Dulce Mercado <soydulceangelina@gmail.com>
2020-02-25 13:37:09 -0500

    resolution callback hell

--- promise/add.js
+++ promise/add.js
@@ -21,8 +21,31 @@ function onError(id){
 
 // llamo a la funcion con el id que me interesa, no hay otro parametro ya que obtengo el valor es llamando el .then, al cual se le pasa una funcion, la cual se ejecuta cuando la promesa se resuelva exitosamente
 getCharacter(1)
-    .then(function(data){
-        console.log(`El personaje 1 es ${data.name}`)
+    .then(data1 => {
+        console.log(`El personaje 1 es ${data1.name}`);
+        return getCharacter(2);
     })
-
+    .then(data2 => {
+        console.log(`El personaje 2 es ${data2.name}`);
+        return getCharacter(3);
+    })
+    .then(data3 => {
+        console.log(`El personaje 3 es ${data3.name}`);
+        return getCharacter(4);
+    })
+    .then(data4 => {
+        console.log(`El personaje 4 es ${data4.name}`);
+        return getCharacter(5);
+    })
+    .then(data5 => {
+        console.log(`El personaje 5 es ${data5.name}`);
+        return getCharacter(6);
+    })
+    .then(data6 => {
+        console.log(`El personaje 6 es ${data6.name}`);
+        return getCharacter(7);
+    })
+    .then(data7 => {
+        console.log(`El personaje 7 es ${data7.name}`);
+    })
 .catch(onError);
